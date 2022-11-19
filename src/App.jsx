ae558710ae4efac06f3bb7c8fa80b2f665a37452
kainpets <pawelstepniak22@gmail.com>
2022-11-19 11:35:21 +0100

    Revert master back to working prototype before form was split into components

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,80 @@ function App() {
       <ExperienceForm />
     </div>
   );
+
+  function handleResetClick() {
+    setAllValues([]);
+    setName("");
+    setEmail("");
+    setNumber("");
+    setSchool("");
+    setCompany("");
+    setPosition("");
+    setStudies("");
+    setBeginDate(new Date());
+    setDateOfStudy(new Date());
+    setEndDate(new Date());
+    setSubmitted(previouslySubmitted => !previouslySubmitted);
+  }
+
+  function handleEndDateChange(e) {
+    setEndDate(e.target.value);
+  }
+
+  function handleBeginDateChange(e) {
+    setBeginDate(e.target.value);
+  }
+
+  function handlePositionChange(e) {
+    setPosition(e.target.value);
+  }
+
+  function handleCompanyChange(e) {
+    setCompany(e.target.value);
+  }
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleNumberChange(e) {
+    setNumber(e.target.value);
+  }
+
+  function handleSchoolChange(e) {
+    setSchool(e.target.value);
+  }
+
+  function handleStudiesChange(e) {
+    setStudies(e.target.value);
+  }
+
+  function handleDateOfStudyChange(e) {
+    setDateOfStudy(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setSubmitted(!submitted);
+    setAllValues([
+      ...allValues,
+      name,
+      email,
+      number,
+      school,
+      studies,
+      dateOfStudy,
+      company,
+      position,
+      beginDate,
+      endDate,
+      submitted,
+    ]);
+  }
 }
 
 export default App;
